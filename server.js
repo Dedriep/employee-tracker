eed65c2625ec36ae61712e6f808c7e990416000e
dedrie p <dedrie@dedries-MacBook-Air.local>
2022-03-22 09:49:13 -0400

    push

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const db = require ('./config/connection')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+db.connect(err => {
+  if (err) throw err;
+  console.log(' CONNECTED');
+  app.listen(PORT, () => {
+    console.log(`PORT ${PORT}`);
+  });
+});
